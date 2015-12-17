936e86f5ff931d84f1409555bfb903cc5e979596
winder <wwinder.unh@gmail.com>
2015-12-17 09:50:11 -0500

    d17

--- d17.py
+++ d17.py
@@ -5,16 +5,14 @@ import itertools
 print 'Number of arguments:', len(sys.argv), 'arguments.'
 print 'Argument List:', str(sys.argv)
 
-containers = open(sys.argv[1]).read().split()
+containers = map(int, open(sys.argv[1]).read().split())
 goalsize = int(sys.argv[2])
 
 combinations = 0
 combinationsMinContainers = False
 for i in range(1,len(containers)+1):
   for perm in itertools.combinations(containers, i):
-    s = reduce(lambda x,y: int(x)+int(y), perm)
-    if s == goalsize:
-      #print perm,"=",s
+    if sum(perm) == goalsize:
       combinations += 1
   if combinations > 0 and not combinationsMinContainers:
     combinationsMinContainers = combinations
